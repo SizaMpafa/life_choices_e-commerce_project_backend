57a03dd4553df7dd94a74754287c3c8c394ee71d
SizaMpafa <sizampafa972@gmail.com>
2026-04-29 07:29:27 +0100

    OIFWNFUHIANHSICB

--- config/config.js
+++ config/config.js
@@ -2,7 +2,11 @@ import mysql from 'mysql2/promise'
 import 'dotenv/config'
 
 const pool = mysql.createPool({
-  uri: process.env.MYSQL_URL
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 })
 
 export { pool }
